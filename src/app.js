c28fc7129076935d99ea4efbc191bbf9819d83d4
Neil Power <neil.power@hootsuite.com>
2015-12-10 14:54:03 -0800

    formatting changes

--- src/app.js
+++ src/app.js
@@ -3,39 +3,39 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux/native';
 
 const {
- Navigator, View, Text, TouchableHighlight, TouchableOpacity, StyleSheet, ProgressViewIOS
-} = React;
+    Navigator, View, Text, TouchableHighlight, TouchableOpacity, StyleSheet, ProgressViewIOS
+    } = React;
 
 var Poll = require('./components/poll.component');
 
 
 class AppRouter extends React.Component {
 
-  renderScene(route, nav) {
-    switch (route.name) {
-      case 'currentQ':
-        return <Poll/>;
-      default:
-        return <View><Text>Hello World</Text></View>;
+    renderScene(route, nav) {
+        switch (route.name) {
+            case 'currentQ':
+                return <Poll/>;
+            default:
+                return <View>
+                    <Text>Hello World</Text>
+                </View>;
+        }
     }
-  }
 
-  render(){
-    return(
-      <Navigator
-       initialRoute={ { name: "currentQ"} }
-       renderScene={this.renderScene.bind(this)}
-       configureScene={ (route) => {
-         if (route.sceneConfig) {
-           return route.sceneConfig;
-         }
-         return Navigator.SceneConfigs.FloatFromRight;
-       }}
-      />
-    );
-  }
+    render() {
+        return (
+            <Navigator
+                initialRoute={ {name: "currentQ"} }
+                renderScene={this.renderScene.bind(this)}
+                configureScene={ (route) => {
+                    if (route.sceneConfig) {
+                        return route.sceneConfig;
+                    }
+                    return Navigator.SceneConfigs.FloatFromRight;
+                }}
+            />
+        );
+    }
 }
-
-
 
 export default AppRouter;
