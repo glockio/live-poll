5b9b4194c3d2295b3bf15184722339809c034082
Martin Jung <martin.jung@hootsuite.com>
2015-12-10 13:53:12 -0800

    implement reducer

--- src/reducers/root.reducer.js
+++ src/reducers/root.reducer.js
@@ -1,24 +1,105 @@
 import {Map, Seq, List,OrderedMap, Record} from 'immutable';
 
 const initialState = Map({
-  loading: false,
-  authData: null,
+  userId: null,
+
+  loading: {
+    isLoading: false,
+    message: "Loading ..."
+  },
+
+  error: {
+    isError: false,
+    message: "error message"
+  },
+
+  openPollID: "sdfdsfsfdsfsd",
+
+  detailPollId: "sdfdsfdsfdsf",
+
+  polls: {
+    poll1Id: {
+      key: "1234",
+      questionText: "Do you like react-native?",
+      closed: false,
+      publishedAt: "1449774745",
+      answers: {
+        "answer1Id": {
+          key: "answer1Id",
+          text: "Yes"
+        },
+        "answer2Id": {
+          key: "answer2Id",
+          text: "No"
+        },
+        "answer3Id": {
+          key: "answer3Id",
+          text: "Maybe So"
+        }
+      }
+    },
+    poll2Id: {
+      key: "1234",
+      questionText: "Do you like react-native?",
+      closed: false,
+      publishedAt: "1449774745",
+      answers: {
+        "answer4Id": {
+          key: "answer1Id",
+          text: "Yes"
+        },
+        "answer5Id": {
+          key: "answer2Id",
+          text: "No"
+        },
+        "answer6Id": {
+          key: "answer3Id",
+          text: "Maybe So"
+        }
+      }
+    }
+  },
+
+  votes: {
+    'answer1Id': {
+      voteId: {userId: "09839083"},
+      voteId: {userId: "394u3098a9"}
+    }
+  }
 });
+
 
 export default function rootReducer(state=initialState, action) {
 
   console.log(`Calling ${action.type}...`);
 
   switch (action.type) {
-    case "SET_LOADING": {
-      // always need to return the new state
-      return state.set('loading', action.loading);
+    case "GET_POLLS": {
+      if (action.loading.isLoading || action.error.isError) {
+        return state.merge({loading: action.loading, error: action.error});
+      } else {
+        return state.merge({polls: action.polls, loading: action.loading, error: action.error});
+      }
     }
-    case "SET_USER_ID": {
-      console.log("setting auth data");
 
+    case "GET_LIVE_POLL_ID": {
+      if (action.loading.isLoading || action.error.isError) {
+        return state.merge({loading: action.loading, error: action.error});
+      } else {
+        return state.merge({openPollID: action.openPollID, loading: action.loading, error: action.error});
+      }
+    }
+
+    case "POST_VOTE": {
+      if (action.loading.isLoading || action.error.isError) {
+        return state.merge({loading: action.loading, error: action.error});
+      } else {
+        return state.merge({votes: action.votes, loading: action.loading, error: action.error});
+      }
+    }
+
+    case "SET_USER_ID": {
       var result =  state.set('userId', action.userId);
-      console.log(result.toJS());
       return result;
     }
   }
