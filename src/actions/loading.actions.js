0c7da05fc3df2ece601b9b0edc71449a52a6eb2b
Jake Cooper <jake.elijah.cooper@gmail.com>
2015-12-10 13:41:52 -0800

    Added basic action functions

--- src/actions/loading.actions.js
+++ src/actions/loading.actions.js
@@ -2,5 +2,20 @@
 
 // For some conventions on actions https://github.com/acdlite/flux-standard-action
 
-export const setLoaing(loading) => {type: "SET_LOADING", loading}
+export function postVote (userId, vote) {
+    return (dispatch) => {
+        dispatch({TYPE: "POST_VOTE"})
+    }
+}
 
+export function getPastPolls () {
+    return (dispatch) => {
+        dispatch({TYPE: "GET_PAST_POLLS"})
+    }
+}
+
+export function getPollById () {
+    return (dispatch) => {
+        dispatch({TYPE: "GET_ONE_POLL"})
+    }
+}
